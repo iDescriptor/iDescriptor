5e7812571922c604f9670e1ec94abdf7f7caa637
uncor3 <contactuncor3@gmail.com>
2026-08-17 17:29:48 +0000

    New translations idescriptor.ts (Portuguese, Brazilian)
    [ci skip]

--- translations/pt.ts
+++ translations/pt.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US" language="pt-PT">
+<TS version="2.1" sourcelanguage="en_US" language="pt-BR">
   <context>
     <name>Airplay</name>
     <message>
