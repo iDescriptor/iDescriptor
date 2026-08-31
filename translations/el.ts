f1838372b027debee0853308ba853a38b2c237c8
uncor3 <contactuncor3@gmail.com>
2026-08-31 14:55:22 +0300

    New translations idescriptor.ts (Greek)
    [ci skip]

--- translations/el.ts
+++ translations/el.ts
@@ -15,6 +15,36 @@
       <translation>Αποτυχία εκκίνησης του AirPlay.</translation>
     </message>
     <message>
+      <location filename="../src/ui/tools/Airplay.qml" line="144"/>
+      <source>Bonjour must be running before AirPlay can start.</source>
+      <translation>Το Bonjour πρέπει να εκτελείται πριν από την εκκίνηση του AirPlay.</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/tools/Airplay.qml" line="145"/>
+      <source>Avahi must be running before AirPlay can start.</source>
+      <translation>Το Avahi πρέπει να εκτελείται πριν από την εκκίνηση του AirPlay.</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/tools/Airplay.qml" line="148"/>
+      <source>Bonjour must be installed before AirPlay can start.</source>
+      <translation>Το Bonjour πρέπει να εγκατασταθεί πριν από την εκκίνηση του AirPlay.</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/tools/Airplay.qml" line="149"/>
+      <source>Avahi must be installed before AirPlay can start.</source>
+      <translation>Το Avahi πρέπει να εγκατασταθεί πριν από την εκκίνηση του AirPlay.</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/tools/Airplay.qml" line="151"/>
+      <source>Unable to check AirPlay requirements: %1</source>
+      <translation>Δεν ήταν δυνατός ο έλεγχος των απαιτήσεων του AirPlay: %1</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/tools/Airplay.qml" line="162"/>
+      <source>Failed to start the AirPlay backend: %1</source>
+      <translation>Αποτυχία εκκίνησης του backend AirPlay: %1</translation>
+    </message>
+    <message>
       <location filename="../src/ui/tools/Airplay.qml" line="163"/>
       <source>Waiting for device connection</source>
       <translation>Αναμονή για σύνδεση συσκευής</translation>
@@ -4561,6 +4591,36 @@ Do you want to continue anyway?</source>
       <location filename="../src/ui/Settings.qml" line="283"/>
       <source>Developer Disk Image Path</source>
       <translation>Διαδρομή εικόνας δίσκου προγραμματιστή</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/Settings.qml" line="308"/>
+      <source>Network discovery</source>
+      <translation>Ανακάλυψη δικτύου</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/Settings.qml" line="318"/>
+      <source>Automatic</source>
+      <translation>Αυτόματα</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/Settings.qml" line="319"/>
+      <source>Built-in</source>
+      <translation>Ενσωματωμένο</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/Settings.qml" line="320"/>
+      <source>Avahi</source>
+      <translation>Avahi</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/Settings.qml" line="321"/>
+      <source>Bonjour</source>
+      <translation>Bonjour</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/Settings.qml" line="330"/>
+      <source>Choose how devices are discovered on the local network.</source>
+      <translation>Επιλέξτε τον τρόπο εντοπισμού συσκευών στο τοπικό δίκτυο.</translation>
     </message>
     <message>
       <location filename="../src/ui/Settings.qml" line="295"/>
